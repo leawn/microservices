40c575412104186f4ca17e4c80dfb5fc3c3c9db7
Leon <leonrubner2809@gmail.com>
2021-02-04 13:07:38 +0100

    added support for query after possible crash commit

---
+++ mini-microservices-app/moderation/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res, next) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.include('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+
+    res.send({});
+});
+
+app.listen(4003, () => {
+    console.log('Listening on 4003');
+});
