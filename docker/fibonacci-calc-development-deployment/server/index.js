d8dd20fa650dd5399ee08cca80b5e0b5a3b5bdf4
Leon <leonrubner2809@gmail.com>
2021-02-18 18:21:12 +0100

    k8s commit

--- docker/fibonacci-calc-development-deployment/server/index.js
+++ docker/fibonacci-calc-development-deployment/server/index.js
@@ -21,7 +21,7 @@ const pgClient = new Pool({
 
 pgClient.on('connect', () => {
     pgClient
-        .query('CREATE TABLE IF NOT EXISTS "values" (number INT)')
+        .query('CREATE TABLE IF NOT EXISTS values (number INT)')
         .catch((err) => console.log(err));
 });
 
