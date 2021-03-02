45175f01f211ddc4cd10e4007620198e9e16fc44
Leon <leonrubner2809@gmail.com>
2021-03-02 17:15:36 +0100

    k8s commit

--- mini-microservices-app/posts/index.js
+++ mini-microservices-app/posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res, next) => {
     res.send(posts);
 });
 
-app.post('/posts', async (req, res, next) => {
+app.post('/posts/create', async (req, res, next) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -23,7 +23,7 @@ app.post('/posts', async (req, res, next) => {
         title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id,
