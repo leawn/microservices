f94313d48b4de546035cef094d3046d4aa16ef42
Leon <leonrubner2809@gmail.com>
2021-03-08 23:04:35 +0100

    auth service commit

--- ticket-market-project/auth/src/routes/current.ts
+++ ticket-market-project/auth/src/routes/current.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get('/api/users/current', (req, res, next) => {
-    res.send('Hi there');
-});
+router.get(
+    '/api/users/current',
+    currentUser,
+    (req: Request, res: Response, next: NextFunction) => {
+        res.send({ currentUser: req.currentUser || null });
+    });
 
 export { router as currentRouter };
--- ticket-market-project/auth/src/routes/signout.ts
+++ ticket-market-project/auth/src/routes/signout.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/signout', (req, res, next) => {
-    res.send('Hi there');
-});
+router.get(
+    '/api/users/signout',
+    (req: Request, res: Response, next: NextFunction) => {
+        req.session = null;
+
+        res.send({});
+    });
 
 export { router as signoutRouter };
