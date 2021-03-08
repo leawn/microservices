7ad0bb033097bd4dd3c2b5f78f820f9656b82806
Leon <leonrubner2809@gmail.com>
2021-03-08 12:26:49 +0100

    authentication process commit

--- ticket-market-project/auth/src/index.ts
+++ ticket-market-project/auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentRouter } from "./routes/current";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -15,8 +18,29 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all('*',async () => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
+const start = async () => {
+    try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000');
+    });
+}
+
+start()
+    .catch(err => {
+        console.log(err);
+    });
--- ticket-market-project/auth/src/routes/signup.ts
+++ ticket-market-project/auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -14,19 +17,25 @@ router.get(
           .isLength({ min: 4, max: 20 })
           .withMessage('Password must be between 4 and 20 characters long')
     ],
-    (req: Request, res: Response, next: NextFunction) => {
+    async (req: Request, res: Response, next: NextFunction) => {
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            throw new Error('Invalid email or password');
+            throw new RequestValidationError(errors.array());
         }
 
         const { email, password } = req.body;
 
-        console.log('Creating a user...');
-        throw new Error('Error connecting to db');
+        const existingUser = await User.findOne({ email });
 
-        res.send({});
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+
+        res.status(201).send(user);
     });
 
 export { router as signupRouter };
