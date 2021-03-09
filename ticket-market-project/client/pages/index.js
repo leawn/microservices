42a7d8ddbe2e757a61416781710180cec60bbd66
Leon <leonrubner2809@gmail.com>
2021-03-09 17:17:46 +0100

    next.js client commit

--- ticket-market-project/client/pages/index.js
+++ ticket-market-project/client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
+import axios from "axios";
+
+
+const LandingPage = () => {
+
     return <h1>Landing page</h1>;
 }
+
+LandingPage.getInitialProps = async () => {
+    const response = await axios.get("api/users/current");
+
+    return response.data;
+};
+
+export default LandingPage;
