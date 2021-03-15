c9d35a860cc14a273a669637dcf644503b02c6bb
Leon <leonrubner2809@gmail.com>
2021-03-15 18:18:48 +0100

    nats test commit

--- ticket-market-project/nats-test/src/publisher.ts
+++ ticket-market-project/nats-test/src/publisher.ts
@@ -2,8 +2,19 @@ import nats from "node-nats-streaming";
 
 const stan = nats.connect("ticketing", "abc", {
     url: "http://localhost:4222"
+    // kubectl port-forward *name of the pod* 4222:4222
 });
 
 stan.on("connect", () => {
-    console.log("Publisher connected to NATS")
+    console.log("Publisher connected to NATS");
+
+    const data = JSON.stringify({
+        id: "123",
+        title: "concert",
+        price: 20
+    });
+
+    stan.publish("ticket:created", data, () => {
+        console.log("Event published");
+    });
 });
