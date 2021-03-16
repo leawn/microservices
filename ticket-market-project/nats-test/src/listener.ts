17f61cfb0789f7199cc6f1d047868ed74009707e
Leon <leonrubner2809@gmail.com>
2021-03-16 02:13:28 +0100

    NATS & commom module update commit

--- ticket-market-project/nats-test/src/listener.ts
+++ ticket-market-project/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 const stan = nats.connect("ticketing", randomBytes(4).toString('hex'), {
     url: "http://localhost:4222"
@@ -13,24 +14,7 @@ stan.on("connect", () => {
         process.exit();
     });
 
-    const options = stan
-        .subscriptionOptions()
-        .setManualAckMode(true);
-    const subscription = stan.subscribe(
-        "ticket:created",
-        "orders-service-queue-group",
-
-    );
-
-    subscription.on("message", (msg: Message) => {
-        const data = msg.getData();
-
-        if (typeof data === "string") {
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-        }
-
-        msg.ack();
-    });
+    new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
