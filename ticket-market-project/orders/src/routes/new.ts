2d5bbe99661d5f5100521059dcfe90501a07c90d
Leon <leonrubner2809@gmail.com>
2021-03-17 23:10:49 +0100

    added ticket & order models for mongoose & events types for common lib

--- ticket-market-project/orders/src/routes/new.ts
+++ ticket-market-project/orders/src/routes/new.ts
@@ -1,9 +1,40 @@
+import mongoose from "mongoose";
 import express, { Request, Response, NextFunction } from "express";
+import { requireAuth, validateRequest, NotFoundError } from "@leawn-tickets-market/common";
+import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response, next: NextFunction) => {
-    res.send({});
-});
+router.post(
+    "/api/orders",
+    requireAuth,
+    [
+        body("ticketId")
+            .not()
+            .isEmpty()
+            .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+            .withMessage("TicketId must be provided")
+    ],
+    validateRequest,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { ticketId } = req.body;
+        // Find the ticket the user is trying to order in the database
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        // Make sure that this ticket is not already reserved
+
+        // Calculate an expiration date for this order
+
+        // Build the order and save it to the database
+
+        // Publish an event saying that an order was created
+        res.send({});
+    }
+);
 
 export { router as newOrderRouter };
