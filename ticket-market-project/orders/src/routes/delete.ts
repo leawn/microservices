3f54db1b08dfc4872cc2bf10442a7518dc56e8f7
Leon <leonrubner2809@gmail.com>
2021-03-18 01:42:56 +0100

    added routes & routes tests commit

--- ticket-market-project/orders/src/routes/delete.ts
+++ ticket-market-project/orders/src/routes/delete.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response, NextFunction } from "express";
+import {NotAuthorizedError, NotFoundError, requireAuth} from "@leawn-tickets-market/common";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response, next: NextFunction) => {
-    res.send({});
-});
+router.delete(
+    "/api/orders/:orderId",
+    requireAuth,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { orderId } = req.params;
+
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+
+        order.status = OrderStatus.Cancelled;
+        await order.save();
+
+        // Publishing an event this was cancelled
+
+        res.status(204).send(order);
+        // probably would be better with a patch or put or post?
+    }
+);
 
 export { router as deleteOrderRouter };
--- ticket-market-project/orders/src/routes/new.ts
+++ ticket-market-project/orders/src/routes/new.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
-import express, { Request, Response, NextFunction } from "express";
-import { requireAuth, validateRequest, NotFoundError } from "@leawn-tickets-market/common";
-import { body } from "express-validator";
-import { Ticket } from "../models/ticket";
-import { Order } from "../models/order";
+import express, {NextFunction, Request, Response} from "express";
+import {BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest} from "@leawn-tickets-market/common";
+import {body} from "express-validator";
+import {Ticket} from "../models/ticket";
+import {Order} from "../models/order";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
     "/api/orders",
@@ -27,13 +29,29 @@ router.post(
         }
 
         // Make sure that this ticket is not already reserved
+        // Run query to look at all orders. Find an order where the ticket
+        // is the ticket we just found *and* the orders status is *not* cancelled
+        // If we find an order from that means the ticket *is* reserved
+        const isReserved = await ticket.isReserved();
+        if (isReserved) {
+            throw new BadRequestError("Ticket is already reserved");
+        }
 
         // Calculate an expiration date for this order
+        const expiration = new Date();
+        expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
         // Build the order and save it to the database
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket
+        });
+        await order.save();
 
         // Publish an event saying that an order was created
-        res.send({});
+        res.status(201).send(order);
     }
 );
 
