cbd37668e1b06b9bd80f82111fc4da58771901c9
Leon <leonrubner2809@gmail.com>
2021-04-09 20:59:18 +0200

    kafkajs producers commit

--- kafka/kafkajs-producer/client.js
+++ kafka/kafkajs-producer/client.js
@@ -51,3 +51,58 @@ const topicMessages = [
 ];
 
 await producer.sendBatch({ topicMessages });
+
+//example w/ custom partitioner
+const MyPartitioner = () => {
+    return ({ topic, partitionMetadata, message }) => {
+        // select a partition based on some logic
+        // return the partition number
+        return 0;
+    }
+}
+
+const PARTITION_METADATA = [
+    { partitionId: 1, leader: 1 },
+    { partitionId: 2, leader: 2 },
+    { partitionId: 0, leader: 0 }
+];
+
+kafka.producer({ createPartitioner: MyPartitioner });
+
+// example w/ default partitioners (requires JavaCompatiblePartitioner!)
+const { Partitioners } = require("kafkajs");
+kafka.producer({ createPartitioner: Partitioners.JavaCompatiblePartitioner });
+
+// example w/ compression
+
+//// GZIP compression protocol
+const { CompressionTypes } = require("kafkajs");
+
+async () => {
+    await producer.send({
+        topic: "topic-name",
+        compression: CompressionTypes.GZIP,
+        messages: [
+            { key: "key1", value: "value1" },
+            { key: "key2", value: "value2" }
+        ]
+    });
+}
+
+//// Snappy compression protocol *npm install --save kafkajs-snappy*
+const { CompressionTypes, CompressionCodecs } = require("kafkajs");
+const SnappyCodec = require("kafkajs-snappy");
+
+CompressionCodecs[CompressionTypes.Snappy] = SnappyCodec;
+
+//// LZ4 compression protocol *npm install --save kafkajs-lz4*
+const { CompressionTypes, CompressionCodecs } = require("kafkajs");
+const LZ4 = require("kafkajs-lz4");
+
+CompressionCodecs[CompressionTypes.LZ4] = new LZ4().codec;
+
+//// ZSTD compression protocol *npm install --save @kafkajs/zstd*
+const { CompressionTypes, CompressionCodecs } = require("kafkajs");
+const ZstdCodec = require("@kafkajs/zstd");
+
+CompressionCodecs[CompressionTypes.ZSTD] = ZstdCodec();
